88e22b306ba70d5c1823e387a4515cffc0c822fa
TAKUYA HIRATA <a.jun.ky@gmail.com>
2013-01-10 15:00:14 +0900

    キャンセル機能を追加しました。 小数点機能を追加するために考察している段階です。

--- Calculator/ViewController.h
+++ Calculator/ViewController.h
@@ -8,6 +8,35 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController{
+    BOOL startInput;
+    double x,y,a;
+    char op;
+}
+
+@property (weak, nonatomic) IBOutlet UILabel *result;
+@property (weak, nonatomic) IBOutlet UILabel *op;
+
+- (IBAction)AC:(id)sender;
+- (IBAction)C:(id)sender;
+- (IBAction)point:(id)sender;
+
+- (IBAction)zero:(id)sender;
+- (IBAction)one:(id)sender;
+- (IBAction)tow:(id)sender;
+- (IBAction)three:(id)sender;
+- (IBAction)four:(id)sender;
+- (IBAction)five:(id)sender;
+- (IBAction)six:(id)sender;
+- (IBAction)seven:(id)sender;
+- (IBAction)eight:(id)sender;
+- (IBAction)nine:(id)sender;
+
+- (IBAction)plus:(id)sender;
+- (IBAction)minus:(id)sender;
+- (IBAction)mul:(id)sender;
+- (IBAction)div:(id)sender;
+- (IBAction)equal:(id)sender;
+
 
 @end
--- Calculator/ViewController.m
+++ Calculator/ViewController.m
@@ -25,5 +25,218 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+    /**********機能**********/
+- (IBAction)AC:(id)sender {
+    x=0;
+    y=0;
+    a=0;
+    [[self result]setText:[NSString stringWithFormat:@"%d",0]];
+    [[self op]setText:[NSString     stringWithFormat:@"　"]];
+    startInput=0;
+
+}
+
+- (IBAction)C:(id)sender {
+    x=0;
+    y=a;
+    [[self result]setText:[NSString stringWithFormat:@"%g",a]];
+    startInput=0;
+}
+
+- (IBAction)point:(id)sender {
+    
+    
+    
+}
+
+
+
+
+
+
+
+    /***********入力**********/
+- (IBAction)zero:(id)sender {x=0;
+    switch(startInput){
+        case 0: //未入力
+            y=x;
+            startInput=1;
+            break;
+        case 1: //既入力
+            y=y*10+x;
+    }
+    [[self result]setText:[NSString stringWithFormat:@"%g",y]];
+}
+
+- (IBAction)one:(id)sender {x=1;
+    switch(startInput){
+        case 0: //未入力
+            y=x;
+            startInput=1;
+            break;
+        case 1: //既入力
+            y=y*10+x;
+    }
+    [[self result]setText:[NSString stringWithFormat:@"%g",y]];
+}
+
+- (IBAction)tow:(id)sender {x=2;
+    switch(startInput){
+        case 0: //未入力
+            y=x;
+            startInput=1;
+            break;
+        case 1: //既入力
+            y=y*10+x;
+    }
+    [[self result]setText:[NSString stringWithFormat:@"%g",y]];
+}
+
+- (IBAction)three:(id)sender{x=3;
+    switch(startInput){
+        case 0: //未入力
+            y=x;
+            startInput=1;
+            break;
+        case 1: //既入力
+            y=y*10+x;
+    }
+    [[self result]setText:[NSString stringWithFormat:@"%g",y]];
+}
+
+
+- (IBAction)four:(id)sender {x=4;
+    switch(startInput){
+        case 0: //未入力
+            y=x;
+            startInput=1;
+            break;
+        case 1: //既入力
+            y=y*10+x;
+    }
+    [[self result]setText:[NSString stringWithFormat:@"%g",y]];
+}
+
+- (IBAction)five:(id)sender {x=5;
+    switch(startInput){
+        case 0: //未入力
+            y=x;
+            startInput=1;
+            break;
+        case 1: //既入力
+            y=y*10+x;
+    }
+    [[self result]setText:[NSString stringWithFormat:@"%g",y]];
+}
+
+- (IBAction)six:(id)sender {x=6;
+    switch(startInput){
+        case 0: //未入力
+            y=x;
+            startInput=1;
+            break;
+        case 1: //既入力
+            y=y*10+x;
+    }
+    [[self result]setText:[NSString stringWithFormat:@"%g",y]];
+}
+
+- (IBAction)seven:(id)sender {x=7;
+    switch(startInput){
+        case 0: //未入力
+            y=x;
+            startInput=1;
+            break;
+        case 1: //既入力
+            y=y*10+x;
+    }
+    [[self result]setText:[NSString stringWithFormat:@"%g",y]];
+}
+
+- (IBAction)eight:(id)sender {x=8;
+    switch(startInput){
+        case 0: //未入力
+            y=x;
+            startInput=1;
+            break;
+        case 1: //既入力
+            y=y*10+x;
+    }
+    [[self result]setText:[NSString stringWithFormat:@"%g",y]];
+}
+
+- (IBAction)nine:(id)sender {x=9;
+    switch(startInput){
+        case 0: //未入力
+            y=x;
+            startInput=1;
+            break;
+        case 1: //既入力
+            y=y*10+x;
+    }
+    [[self result]setText:[NSString stringWithFormat:@"%g",y]];
+}
+
+    /**********演算**********/
+- (IBAction)plus:(id)sender {op='+';
+    [[self op]setText:[NSString     stringWithFormat:@"+"]];
+    a=y;
+    startInput=0;
+}
+- (IBAction)minus:(id)sender {op='-';
+    [[self op]setText:[NSString     stringWithFormat:@"-"]];
+    a=y;
+    startInput=0;
+}
+- (IBAction)mul:(id)sender {op='*';
+    [[self op]setText:[NSString     stringWithFormat:@"×"]];
+    a=y;
+    startInput=0;
+}
+- (IBAction)div:(id)sender {op='/';
+    [[self op]setText:[NSString     stringWithFormat:@"÷"]];
+    a=y;
+    startInput=0;
+}
+    /*********結果**********/
+- (IBAction)equal:(id)sender {
+    switch (op) {
+            
+        case '+':
+            y=a+y;
+            [[self result]setText:[NSString stringWithFormat:@"%g",y]];
+            break;
+            
+        case '-':
+            y=a-y;
+            [[self result]setText:[NSString stringWithFormat:@"%g",y]];
+            break;
+            
+        case '*':
+            y=a*y;
+            [[self result]setText:[NSString stringWithFormat:@"%g",y]];
+            break;
+            
+        case '/':
+            y=a/y;
+            [[self result]setText:[NSString stringWithFormat:@"%g",y]];
+            break;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 @end
